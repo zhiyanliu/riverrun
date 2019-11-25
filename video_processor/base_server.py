0aa7d291c8d59554b3fd673be46738d890ff70db
Zhi Yan Liu <liuzhiya@amazon.com>
2019-11-25 22:47:42 +0800

    Add logging interval parameter to server

--- video_processor/base_server.py
+++ video_processor/base_server.py
@@ -3,13 +3,14 @@ import threading
 
 
 class Server:
-    def __init__(self, name):
+    def __init__(self, name, log_interval_sec=5):
         self._name = name
         self._stop_flag = multiprocessing.Event()
+        self._log_interval = log_interval_sec
         self._start_log_routine()
 
     def _start_log_routine(self):
-        self._log_timer = threading.Timer(5, self._log_routine)
+        self._log_timer = threading.Timer(self._log_interval, self._log_routine)
         self._log_timer.start()
 
     def _log_routine(self):
